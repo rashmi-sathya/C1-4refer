f3f624972745eea4a7c82d7562c30d134cdc7917
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-03-23 23:03:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 var colour = ["red","blue","green","purple","yellow","orange","black"];
+var colour1 = ["red","red","red","red","red","red","red"];
+var colour2 = ["blue","blue","blue","blue","blue","blue","blue"];
 function draw() {
 background("#E0F9FB");
 
@@ -12,19 +14,19 @@ var y=30;
 for (var k = 0; k < colour.length; k++) {
   fill(colour[k]);
   
-  ellipse(x, y, 50, 50);
+ ellipse(x, y, 50, 50);
   y=y+50;
 }
-for (var i = 0; i < colour.length; i++) {
-  fill(colour[i]);
+for (var i = 0; i < colour1.length; i++) {
+ fill(colour1[i]);
   
 
   ellipse(x1, y1, 50, 50);
   x1 = x1 + 50;
   y1=y1+50;
 }
-  for (var j =0; j < colour.length; j++) {
-  fill(colour[j]);
+  for (var j =0; j < colour2.length; j++) {
+  fill(colour2[j]);
   
   
   ellipse(x2, y2, 50, 50);
